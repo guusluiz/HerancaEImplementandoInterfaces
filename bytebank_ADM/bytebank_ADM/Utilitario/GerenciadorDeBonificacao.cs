a6042ab767579de84b859649f69f111887f6df34
Gustavo Luiz <gustavoluizmiranda405@gmail.com>
2022-10-18 23:35:08 -0300

    Initial commit

---
+++ bytebank_ADM/bytebank_ADM/Utilitario/GerenciadorDeBonificacao.cs
@@ -0,0 +1,23 @@
+﻿using bytebank_ADM.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytebank_ADM.Utilitario
+{
+    public class GerenciadorDeBonificacao
+    {
+        public double TotalDeBonifacacao { get; private set; }
+
+        public void Registrar(Funcionario funcionario)
+        {
+            this.TotalDeBonifacacao += funcionario.GetBonificacao();
+        }
+        public void Registrar(Diretor diretor)
+        {
+            this.TotalDeBonifacacao += diretor.GetBonificacao();
+        }
+    }
+}
